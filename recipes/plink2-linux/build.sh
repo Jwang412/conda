00451dd303d9cd13af9392f381be5c9276e3f8ae
snewhouse <stephen.j.newhouse@gmail.com>
2016-08-29 15:36:51 +0100

    renamed to plink2-linux

---
+++ recipes/plink2-linux/build.sh
@@ -0,0 +1,6 @@
+mkdir -p $PREFIX/bin
+#unzip plink_linux_x86_64_dev.zip
+chmod 755 plink
+chmod 755 prettify
+cp plink $PREFIX/bin
+cp prettify $PREFIX/bin
--- recipes/plink2/build.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p $PREFIX/bin
-unzip plink_linux_x86_64_dev.zip
-cp plink $PREFIX/bin
-cp prettify $PREFIX/bin
